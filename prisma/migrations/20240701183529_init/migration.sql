f0a0dd17d3a2fb67b4bb776f7dce310e1f289424
Alexis <d-alexis@hotmail.fr>
2024-07-07 10:53:38 +0200

    MAJ

--- prisma/migrations/20240701183529_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `Report_userId_fkey` ON `report`;
-
--- AddForeignKey
-ALTER TABLE `Report` ADD CONSTRAINT `Report_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
