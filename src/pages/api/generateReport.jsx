6f70212c7f386b5160ddc691f9bb3d2c18c8236a
Alexis <d-alexis@hotmail.fr>
2024-07-07 22:12:53 +0200

    MAJ

--- src/pages/api/generateReport.jsx
+++ src/pages/api/generateReport.jsx
@@ -54,15 +54,6 @@ function getUserIdFromToken(token) {
 
 export default async function handler(req, res) {
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-    if (req.method === 'OPTIONS') {
-      res.status(200).end();
-      return;
-    }
-
     
     if (req.method === "POST") {
         const { email, url } = req.body;
