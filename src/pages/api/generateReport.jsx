1862036238e09d874054451a9a70bdba58c6e00e
Alexis <d-alexis@hotmail.fr>
2024-07-07 23:22:01 +0200

    Test2 pour résoudre l'erreur le déploiement vercel

--- src/pages/api/generateReport.jsx
+++ src/pages/api/generateReport.jsx
@@ -54,6 +54,9 @@ function getUserIdFromToken(token) {
 
 export default async function handler(req, res) {
 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     
     if (req.method === "POST") {
         const { email, url } = req.body;
