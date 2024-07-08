17ae439d31ef11362756fae252a2716da21db776
Alexis <d-alexis@hotmail.fr>
2024-07-08 11:36:11 +0200

    maj

--- src/pages/api/generateReport.jsx
+++ src/pages/api/generateReport.jsx
@@ -72,13 +72,6 @@ export default async function handler(req, res) {
 
     await runMiddleware(req, res, cors);
 
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
     
     if (req.method === "POST") {
         const { email, url } = req.body;
