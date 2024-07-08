2d87fd5659dcf0844293f3df0bfb3af283c49672
Alexis <d-alexis@hotmail.fr>
2024-07-08 11:04:20 +0200

    maj

--- src/app/generate_report/page.jsx
+++ src/app/generate_report/page.jsx
@@ -25,7 +25,7 @@ export default function GenerateReport() {
       // Récupération du token dans le local storage
       const token = localStorage.getItem('token');
       // Envoi de la requête HTTP au serveur pour générer le rapport
-      const response = await fetch("/api/generateReport", {
+      const response = await fetch("http://localhost:3000/api/generateReport", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
